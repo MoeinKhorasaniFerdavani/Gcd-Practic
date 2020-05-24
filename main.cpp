d9476a3cb758ff5ce09773cbc1acd41369af4e29
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-05-24 13:01:15 +0430

    add color

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@
 #include <iostream>
 
 #include "functions.h"
-
+#include "d:\moein proje\source\repos\my library\graphic\color.h"
 using namespace std;
 
 int main()
 {
 	int a, b;
+	setcolor(6);
 	cout << "Enter first posetive intiger\n";
+	setcolor(7);
 	cin >> a;
+	setcolor(6);
 	cout<<endl<<"Enter first posetive intiger\n";
+	setcolor(7);
 	cin >> b;
+	setcolor(2);
 	cout << endl <<"your result is:\t"<< gcd(a, b)<<endl;
-
+	setcolor(7);
 	system("pause");
 	return 0;
 
