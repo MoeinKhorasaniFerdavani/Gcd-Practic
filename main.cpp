c0f4ca354fcc199a8f5cee4ff1a4972394278e78
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-05-24 14:26:50 +0430

    add link

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@
 
 #include "d:\moein proje\source\repos\my library\graphic\color.h"
 #include "functions.h"
-
+#include "d:\moein proje\source\repos\my library\web\internet.h"
 using namespace std;
 
 int main()
 {
 	int a, b;
+	string answer;
 	printc(4, "Welcom to program\n");
 	printc(6, "Enter first intiger\n");
 	cin >> a;
@@ -35,6 +36,11 @@ int main()
 	else
 		printc(4, "\t is not prime\n");
 
+	printc(3,"\ndo you want to comminucate to programer?\n");
+	getchar();
+	cin >> answer;
+	if (answer == "yes" || answer == "y")
+		link("https://iut.ac.ir/");
 	cout << endl << endl;
 	system("pause");
 	return 0;
