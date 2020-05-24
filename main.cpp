d6606ac45176e6272be52d30d77e3a1a65d4f50f
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-05-24 12:24:01 +0430

    make main.cpp this file have main function

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//	 IN THE NAME OF GOD
+
+#include <iostream>
+
+#include "functions.h"
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << endl << gcd(a, b)<<endl;
+
+	system("pause");
+	return 0;
+
+}
